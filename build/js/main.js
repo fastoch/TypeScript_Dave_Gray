2da86426bdd692b334aeaffcae67cd3cfec5c1bc
Fastoch <89261095+fastoch@users.noreply.github.com>
2023-10-16 14:27:20 +0200

    Add files via upload

--- build/js/main.js
+++ build/js/main.js
@@ -1,4 +1,20 @@
 "use strict";
+<<<<<<< HEAD
+// convert to a more or less specific type
+let a = 'hello';
+let b = a; // less specific
+let c = a; // more specific
+let d = 'world';
+let e = 'random';
+const addOrConcat = (a, b, c) => {
+    if (c === 'add')
+        return a + b;
+    return '' + a + b; // The empty string coerces data type to string
+};
+let myVal = addOrConcat(2, 2, 'concat');
+let nextVal = addOrConcat(2, 2, 'add');
+const img = document.getElementById('img');
+=======
 // type is inferred
 let myName = 'fastoch';
 // type is explicit
@@ -44,3 +60,4 @@ var Grade;
     Grade[Grade["B"] = 4] = "B";
     Grade[Grade["A"] = 5] = "A";
 })(Grade || (Grade = {}));
+>>>>>>> 9c589f470161c9f371f4aaa5c7a717479cb48be1
