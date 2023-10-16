776a4f1fe2942ecf063d840920e3d10b337b3a53
fastoch <fastoch@ik.me>
2023-10-16 14:59:29 +0200

    chapter 6 - 128min

--- build/js/main.js
+++ build/js/main.js
@@ -1,15 +1,1 @@
 "use strict";
-// convert to a more or less specific type
-let a = 'hello';
-let b = a; // less specific
-let c = a; // more specific
-let d = 'world';
-let e = 'random';
-const addOrConcat = (a, b, c) => {
-    if (c === 'add')
-        return a + b;
-    return '' + a + b; // The empty string coerces data type to string
-};
-let myVal = addOrConcat(2, 2, 'concat');
-let nextVal = addOrConcat(2, 2, 'add');
-const img = document.getElementById('img');
