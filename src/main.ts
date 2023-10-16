f91dcd814e201d2fbfd9dad47422c79cbf82508a
Fastoch <89261095+fastoch@users.noreply.github.com>
2023-10-16 14:27:44 +0200

    Add files via upload

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,26 @@
+<<<<<<< HEAD
+type One = string
+type Two = string | number
+type Three = 'hello'
+
+// convert to a more or less specific type
+let a: One = 'hello'  
+let b = a as Two      // less specific
+let c = a as Three    // more specific
+
+let d = <One>'world'
+let e = <string | number>'random'
+
+const addOrConcat = (a: number, b: number, c: 'add' | 'concat'): number | string => {
+  if (c === 'add') return a + b
+  return '' + a + b   // The empty string coerces data type to string
+}
+
+let myVal: string = addOrConcat(2,2,'concat') as string
+let nextVal: number = addOrConcat(2,2,'add') as number
+
+const img = document.getElementById('img') as HTMLImageElement
+=======
 // type is inferred
 let myName = 'fastoch'
 
@@ -61,3 +84,4 @@ enum Grade {
   B,
   A
 }
+>>>>>>> 9c589f470161c9f371f4aaa5c7a717479cb48be1
