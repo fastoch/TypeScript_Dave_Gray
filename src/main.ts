f61bd3c5bfc8188976cf293675fc883161075ae9
fastoch <fastoch@ik.me>
2023-10-16 15:07:00 +0200

    chapter 6 - 132min

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+class Coder {
+  name: string 
+  music: string
+  age: number
+  lang: string
+
+  constructor(name: string, music: string, age: number, lang: string) {
+    this.name = name
+    this.music = music
+    this.age = age
+    this.lang = lang
+  }
+}
+
