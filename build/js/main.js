76f1fc0be1a78dc155aa126a100242e3afbbc95e
fastoch <fastoch@ik.me>
2023-10-16 14:22:46 +0200

    chapter 5 - 120min

--- build/js/main.js
+++ build/js/main.js
@@ -1,3 +1,15 @@
 "use strict";
-let username = 'fastoch pustoch';
-console.log(username);
+// convert to a more or less specific type
+let a = 'hello';
+let b = a; // less specific
+let c = a; // more specific
+let d = 'world';
+let e = 'random';
+const addOrConcat = (a, b, c) => {
+    if (c === 'add')
+        return a + b;
+    return '' + a + b; // The empty string coerces data type to string
+};
+let myVal = addOrConcat(2, 2, 'concat');
+let nextVal = addOrConcat(2, 2, 'add');
+const img = document.getElementById('img');
