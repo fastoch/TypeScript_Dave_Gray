ee61f5846e0daa4ea197a7a8e42109ac2fbe85c6
fastoch <fastoch@ik.me>
2023-12-10 09:17:25 +0100

    end of chapter 9

--- src/main.ts
+++ src/main.ts
@@ -92,4 +92,33 @@ console.log(tsAssign);
 // Parameters
 
 type AssignParams = Parameters<typeof createNewAssign>
+const assignArgs: AssignParams = ["Generics", 100]
+
+const tsAssign2: NewAssign = createNewAssign(...assignArgs)
+console.log(tsAssign2);
+
+// Awaited
+
+interface User {
+  id: number,
+  name: string,
+  username: string,
+  email: string
+}
+
+const fetchUsers = async(): Promise<User[]> => {
+  const data = await fetch(
+    'https://jsonplaceholder.typicode.com/users'
+  ).then(res => {
+    return res.json()
+  }).catch(err => {
+    if(err instanceof Error) console.log(err.message);
+  })
+  return data
+}
+
+type FetchUsersReturnType = ReturnType<typeof fetchUsers>
+type AwaitedFetchUsersReturnType = Awaited<ReturnType<typeof fetchUsers>>
+
+fetchUsers().then(users => console.log(users))
 
